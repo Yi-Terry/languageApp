f059d7d63f5ed40647b8f70e3b8f90b345827380
Terry Yi <91101203+Yi-Terry@users.noreply.github.com>
2024-04-17 22:16:53 -0400

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:language_app/language_app.dart';
 
-import 'package:home_page/my_home_page.dart';
-
-void main() { 
-  runApp(MyApp());
+void main() {
+  runApp(
+      const LanguageApp()
+  );
 }
 
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BL Home Page',
-      debugShowCheckedModeBanner: false,
-
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue, 
-      ), 
-      home: MyHomePage(),
-    );
-  }
-}
