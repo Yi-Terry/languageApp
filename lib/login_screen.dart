891ef6332b5a94b54b98af388a345c21c3e75c77
Chris D <cdorin@nyit.edu>
2024-04-25 00:24:01 -0400

    Graphical bug fixed

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -21,6 +21,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('Login'),
       ),
