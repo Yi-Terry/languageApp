205f4fa2ca59f3193cc1a3ed850a1f1d65e51907
MarcusFredericks2021 <marcusfredericks2021@gmail.com>
2024-04-25 21:26:38 -0400

    resolving

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 //answer buttons for each quiz question @Kelly o
 
-class AnswerButton extends StatelessWidget{
+class AnswerButton extends StatelessWidget {
   const AnswerButton({
     super.key,
     required this.answerText,
@@ -13,21 +13,20 @@ class AnswerButton extends StatelessWidget{
   final void Function() onTap;
 
   @override
-  Widget build(context){
+  Widget build(context) {
     return ElevatedButton(
-      onPressed: onTap, 
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(
-          vertical: 10,
-          horizontal: 40,
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 40,
+          ),
+          backgroundColor: Color.fromARGB(160, 39, 255, 60),
+          foregroundColor: Colors.white,
         ),
-        backgroundColor: Color.fromARGB(160, 39, 255, 60),
-        foregroundColor: Colors.white,
-      ),
-      child: Text(
-        answerText,
-        textAlign: TextAlign.center,
-        )
-    );
+        child: Text(
+          answerText,
+          textAlign: TextAlign.center,
+        ));
   }
 }
